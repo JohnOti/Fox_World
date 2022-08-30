2a4aabe93d61cddd4abfbd40fe973b661ab017da
John Oduor Otieno <johnoduor222@gmail.com>
2022-08-30 03:23:12 -0700

    Fetched the API data

--- index.js
+++ index.js
@@ -3,3 +3,16 @@
 //   $('.active').removeClass('active');
 //   $(this).addClass('active');
 // });
+const cardImage=document.getElementById("card-image")
+const likeCount=document.getElementById("like-count")
+const cardTitle=document.getElementById("card-title")
+const commentsList=document.getElementById("comments-list")
+const url='https://randomfox.ca/floof/'
+
+fetch(url)
+    .then((response) => response.json())
+    .then(renderGram);
+      
+    function renderGram(data){
+      cardImage.src=data.image
+    }
