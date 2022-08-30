96a3f6c4539ede9b6f72ecd841175b378b8d8126
John Oduor Otieno <johnoduor222@gmail.com>
2022-08-30 11:52:09 -0700

    Added event Listeners

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
-// $('.like, .dislike').on('click', function(event) {
-//   event.preventDefault();
-//   $('.active').removeClass('active');
-//   $(this).addClass('active');
-// });
+document.addEventListener('DOMContentLoaded', (event)=>{
+const imageCard=document.getElementById("imageCard")
+
 const cardImage=document.getElementById("card-image")
 const likeCount=document.getElementById("like-count")
 const cardTitle=document.getElementById("card-title")
 const commentsList=document.getElementById("comments-list")
-const url='https://randomfox.ca/floof/'
+const addLike=document.getElementById("add-like")
+const disLike=document.getElementById("remove-like")
 
-fetch(url)
+const url='https://randomfox.ca/floof/'
+let likes = 0;
+
+const imageRot=fetch(url)
     .then((response) => response.json())
-    .then(renderGram);
+    .then(renderImage);
       
-    function renderGram(data){
+    function renderImage(data){
       cardImage.src=data.image
+      window.onload = function () {
+        setInterval(change, 5000);
+    };
     }
+
+  
+
+addLike.addEventListener("click",()=>{
+  
+  likes+=1
+  renderLikes();
+})
+
+function renderLikes(){
+  likeCount.textContent=`${likes} likes`
+}
+
+disLike.addEventListener("click",()=>{
+  if(likes>0){
+  likes-=1}
+  renderDisLike();
+})
+
+
+function renderDisLike(){
+  likeCount.textContent=`${likes} likes`
+}
+});
+
